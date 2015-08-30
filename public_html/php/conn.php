f87cb7b8182509b5c9ae0d22f0f1666c7f02a349
Craig Lovelock <craiglovelock54@homail.co.uk>
2015-08-30 22:42:43 +0100

    remove spaces dsn

--- public_html/php/conn.php
+++ public_html/php/conn.php
@@ -6,17 +6,13 @@ ini_set( "display_errors", 1 );
 // all connection information for PDO
 $domain = $_SERVER['HTTP_HOST'];
 
-	$host = '127.0.0.1';
+	$host = 'localhost';
 	$dbname = 'doseofst_every_hour';
 	$username = 'doseofst_craig';
 	$password = 'wireless';
 
 try {
-	$conn = new PDO(
-		"mysql:host = $host;dbname=$dbname",
-		$username,
-		$password
-	);
+	$conn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
 	// show errors
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
