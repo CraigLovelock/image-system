78ab28961b6407a0ef4a17541f4d37cdd19246a6
Craig Lovelock <craiglovelock54@homail.co.uk>
2015-08-30 22:50:58 +0100

    dif conn

--- public_html/php/conn.php
+++ public_html/php/conn.php
@@ -6,18 +6,21 @@ ini_set( "display_errors", 1 );
 // all connection information for PDO
 $domain = $_SERVER['HTTP_HOST'];
 
-	$host = 'localhost';
-	$dbname = 'doseofst_every_hour';
-	$username = 'doseofst_craig';
+if ($domain == 'localhost') {
+	$dsn = 'mysql:dbname=every_hour;host=127.0.0.1';
+	$user = 'root';
+	$password = 'root';
+} else {
+	$dsn = 'mysql:dbname=doseofst_every_hour;host=127.0.0.1';
+	$user = 'doseofst_craig';
 	$password = 'wireless';
+}
 
 try {
-	$conn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
-	// show errors
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch(PDOException $e) {
-	echo 'ERROR: ' . $e->getMessage();
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
 }
 
 ?>
