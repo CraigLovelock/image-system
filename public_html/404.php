d28eb224101548517dd92baa24cdd333714113eb
Craig Lovelock <craiglovelock54@homail.co.uk>
2015-08-30 22:14:30 +0100

    database check env and 404 added

---
+++ public_html/404.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<?php
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+?>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Dose Of Stance</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+	<link rel="stylesheet" href="/assets/css/main.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<?php include ('includes/_header.php'); ?>
+
+	<div style="text-align: center; color: white; width: 100%; top: 45%; position: absolute">
+		<h1>Sorry, we cannot find the page you are looking for :(</h1>
+		<h3>Think this is an error? Let us know by using the contact link</h3>
+	</div>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<script src="/assets/js/app.js"></script>
+</body>
+</html>
--- public_html/php/conn.php
+++ public_html/php/conn.php
@@ -4,10 +4,19 @@ error_reporting( E_ALL );
 ini_set( "display_errors", 1 );
 
 // all connection information for PDO
-$host = '127.0.0.1';
-$dbname = 'every_hour';
-$username = 'root';
-$password = 'root';
+$domain = $_SERVER['HTTP_HOST'];
+
+if ($domain == 'localhost') {
+	$host = '127.0.0.1';
+	$dbname = 'every_hour';
+	$username = 'root';
+	$password = 'root';
+} else {
+	$host = '127.0.0.1';
+	$dbname = 'every_hour';
+	$username = 'doseofst_craig';
+	$password = 'wireless';
+}
 
 try {
 	$conn = new PDO(
@@ -19,7 +28,7 @@ try {
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
-	echo 'ERROR: ' . $e->getMessage();
+	exit('There has been errors.');
 }
 
 ?>
